5d20d427f0bdc7df593ea1824e6dabfd7ce7cc50
Riad Mohamed <riaduk1@gmail.com>
2020-10-15 22:15:55 +0200

    Create NewsListTableVC class.
    Add a new TableViewController in main.sb and set it's class to NewsListTableVC.
    
    Embed the tableVC to NavigationVC

---
+++ GoodNews/Controller/NewsListTableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  NewsListTableViewController.swift
+//  GoodNews
+//
+//  Created by Riad Mohamed on 10/15/20.
+//
+
+import Foundation
+import UIKit
+
+class NewsListTableViewController: UITableViewController {
+    
+}
