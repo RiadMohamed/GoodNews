4a2831311afc97dd9faf7fb7efecc5b9654b04aa
Riad Mohamed <riaduk1@gmail.com>
2020-10-16 22:54:12 +0200

    Create the WebService class and add the getArticles function

---
+++ GoodNews/Services/Service.swift
@@ -0,0 +1,8 @@
+//
+//  Service.swift
+//  GoodNews
+//
+//  Created by Riad Mohamed on 10/16/20.
+//
+
+import Foundation
---
+++ GoodNews/Services/WebService.swift
@@ -0,0 +1,14 @@
+//
+//  WebService.swift
+//  GoodNews
+//
+//  Created by Riad Mohamed on 10/16/20.
+//
+
+import Foundation
+
+class WebService {
+    func getArticles(url: URL, completionHandler: ([Article]?) ->()) {
+        
+    }
+}
