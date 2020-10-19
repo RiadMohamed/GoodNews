12dfe448422500ee4145cd32e153303e5c0b4d6c
Riad Mohamed <riaduk1@gmail.com>
2020-10-19 20:39:25 +0200

    Create the Article cell class and custom cell.
    Link the outlets and the view.

---
+++ GoodNews/Cells/ArticleTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  ArticleTableViewCell.swift
+//  GoodNews
+//
+//  Created by Riad Mohamed on 10/19/20.
+//
+
+import Foundation
+import UIKit
+
+class ArticleTabelViewCell: UITableViewCell {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+}
